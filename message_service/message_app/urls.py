0561bb874685b853316cdf6306a662137c31623d
Mikael Jonsson <mikael@nerdvana.se>
2024-09-27 13:51:46 +0200

    Support format suffixes and switch to API views

--- message_service/message_app/urls.py
+++ message_service/message_app/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
 from message_app import views
 
 urlpatterns = [
     path('messages/', views.message_list),
     path('messages/<int:pk>/', views.message_detail),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- message_service/message_app/views.py
+++ message_service/message_app/views.py
@@ -1,17 +1,61 @@
-from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.parsers import JSONParser
+# from django.shortcuts import render
+# from django.http import HttpResponse, JsonResponse
+# from django.views.decorators.csrf import csrf_exempt
+# from rest_framework.parsers import JSONParser
+
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from message_app.models import Message
 from message_app.serializers import MessageSerializer
 
 # Create your views here.
-
-@csrf_exempt
-def message_list(request):
+@api_view(['GET', 'POST'])
+def message_list(request, format=None):
     """
     List all messages, or create a new message.
     """
+    if request.method == 'GET':
+        messages = Message.objects.all()
+        serializer = MessageSerializer(messages, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = MessageSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+@api_view(['GET', 'PUT', 'DELETE'])
+def message_detail(request, pk, format=None):
+    """
+    Retrieve, update or delete a message.
+    """
+    try:
+        message = Message.objects.get(pk=pk)
+    except Message.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = MessageSerializer(message)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = MessageSerializer(message, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        message.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
+"""
+@csrf_exempt
+def message_list(request):
+    # List all messages, or create a new message.
     if request.method == 'GET':
         message = Message.objects.all()
         serializer = MessageSerializer(message, many=True)
@@ -27,9 +71,7 @@ def message_list(request):
     
 @csrf_exempt
 def message_detail(request, pk):
-    """
-    Retrieve, update or delete a message.
-    """
+    # Retrieve, update or delete a message.
     try:
         message = Message.objects.get(pk=pk)
     except Message.DoesNotExist:
@@ -50,3 +92,4 @@ def message_detail(request, pk):
     elif request.method == 'DELETE':
         message.delete()
         return HttpResponse(status=204)
+"""
